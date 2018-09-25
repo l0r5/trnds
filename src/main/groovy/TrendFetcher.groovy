eefa82f86d3c1aa6420a8612011335194f14f4e7
l0r5 <lars-braitinger@web.de>
2018-09-25 21:21:20 +0200

    change to readFile

--- src/main/groovy/TrendFetcher.groovy
+++ src/main/groovy/TrendFetcher.groovy
@@ -15,7 +15,7 @@ node('trnds') {
 
         stage('Initialization') {
             echo "Initialize..."
-            String apiDataFile = readFile(file: "${env.WORKSPACE}/resources/api_data.json")
+            String apiDataFile = readFile(file: "/resources/api_data.json")
             apiData = new JsonSlurper().parseText(apiDataFile)
         }
 
