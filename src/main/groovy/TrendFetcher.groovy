d3757d2074257f3360d9c0366c82cfda81c64dfd
l0r5 <lars-braitinger@web.de>
2018-10-02 22:25:04 +0200

    change process youtube data

--- src/main/groovy/TrendFetcher.groovy
+++ src/main/groovy/TrendFetcher.groovy
@@ -79,9 +79,8 @@ def processYouTubeData() {
 
     fetchedYouTubeData.items.snippet.eachWithIndex { it, count ->
 
-        def sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX")
         def video = [
-                "publishedAt": sdf.format(new Date(it.publishedAt.toString())),
+                "publishedAt": Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSSX",it.publishedAt.toString()),
                 "title"      : it.title,
                 "description": it.description,
                 "url"        : "https://www.youtube.com/watch?v=" + fetchedYouTubeData.items[count].id.toString()
